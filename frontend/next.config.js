d5abb33b4aca8655837ce61b37c32c57879ceafa
stephenjarso <stephenjacob815@gmail.com>
2026-06-11 01:17:32 +0300

    feat: update Next.js configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: 'http://localhost:8080/api/v1/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
